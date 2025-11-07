619739ae146c28da6665d843966f9c39177887f2
Leonard Schüngel <lschuengel@acardo.com>
2025-11-07 10:04:02 +0100

    Only compile sample if chapter has changed

--- egui-preproc/src/processor.rs
+++ egui-preproc/src/processor.rs
@@ -1,13 +1,15 @@
-use std::fs::OpenOptions;
+use std::fs::{self, OpenOptions};
 use std::io::Write;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
+use std::time::UNIX_EPOCH;
 
 use anyhow::Error;
-use chrono::Local;
+use chrono::{DateTime, Local, Utc};
 use mdbook::book::{Book, Chapter};
 use mdbook::preprocess::{Preprocessor, PreprocessorContext};
 use regex::Regex;
-use tracing::info;
+use tracing::{debug, info};
 
 /// A no-op preprocessor.
 pub struct EguiPreproc;
@@ -47,46 +49,67 @@ impl Preprocessor for EguiPreproc {
 }
 
 fn process_chapter(chapter: &mut Chapter) {
+    if chapter.is_draft_chapter() {
+        return;
+    }
+
+    debug!("Chapter '{}' processing now", chapter.name);
+
+    let chapter_last_modified = {
+        let mut total_path = PathBuf::new();
+        total_path.push("src");
+        total_path.extend(
+            chapter
+                .source_path
+                .as_ref()
+                .expect("Chapter should have a source file"),
+        );
+        last_modified_of(total_path)
+    };
+
     let re = Regex::new(r"\{\{sample: (.*)\}\}").expect("Regex should compile");
     chapter.content = re
         .replace_all(&chapter.content, |caps: &regex::Captures| {
             let sample_name = &caps[1];
+            debug!("Sample: '{sample_name}' processing now");
+            if should_compile(sample_name, &chapter_last_modified) {
+                info!(
+                    "Compiling sample '{}' in chapter '{}'",
+                    sample_name, chapter.name
+                );
+                Command::new("cargo")
+                    .args([
+                        "build",
+                        "--manifest-path",
+                        "./rust/Cargo.toml",
+                        "--bin",
+                        sample_name,
+                        "--release",
+                        "--target",
+                        "wasm32-unknown-unknown",
+                    ])
+                    .stdout(Stdio::inherit())
+                    .stderr(Stdio::inherit())
+                    .output()
+                    .expect("Cargo build failed to start");
 
-            info!("Compiling sample: {sample_name}");
+                info!("Running wasm-bindgen");
 
-            Command::new("cargo")
-                .args([
-                    "build",
-                    "--manifest-path",
-                    "./rust/Cargo.toml",
-                    "--bin",
-                    sample_name,
-                    "--release",
-                    "--target",
-                    "wasm32-unknown-unknown",
-                ])
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .output()
-                .expect("Cargo build failed to start");
-
-            info!("Running wasm-bindgen");
-
-            Command::new("wasm-bindgen")
-                .args([
-                    &format!("./rust/target/wasm32-unknown-unknown/release/{sample_name}.wasm"),
-                    "--target",
-                    "web",
-                    "--out-dir",
-                    "./src/wasm",
-                    "--no-typescript",
-                ])
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .output()
-                .expect("Cannot run wasm-bindgen");
-            info!("Done running wasm-bindgen, printing output now");
-
+                Command::new("wasm-bindgen")
+                    .args([
+                        &format!("./rust/target/wasm32-unknown-unknown/release/{sample_name}.wasm"),
+                        "--target",
+                        "web",
+                        "--out-dir",
+                        "./src/wasm",
+                        "--no-typescript",
+                    ])
+                    .stdout(Stdio::inherit())
+                    .stderr(Stdio::inherit())
+                    .output()
+                    .expect("Cannot run wasm-bindgen");
+                info!("Done running wasm-bindgen");
+            }
             format!(
                 "
 <canvas id='{sample_name}' class='egui-sample'></canvas>
@@ -100,7 +123,46 @@ fn process_chapter(chapter: &mut Chapter) {
             )
         })
         .to_string();
-    info!("done processing chapter {}", chapter.name);
+}
+
+fn should_compile(sample_name: &str, chapter_last_modified: &DateTime<Utc>) -> bool {
+    let output_path = PathBuf::from("src/wasm");
+    let wasm_path = output_path.join(format!("{sample_name}_bg.wasm"));
+    if !fs::exists(&wasm_path).is_ok_and(|t| t) {
+        info!("file '{wasm_path:?}' does not exist, need to compile");
+        return false;
+    }
+
+    let js_path = output_path.join(format!("{sample_name}.js"));
+    if !fs::exists(&js_path).is_ok_and(|t| t) {
+        info!("file '{js_path:?}' does not exist, need to compile");
+        return false;
+    }
+
+    let wasm_last_modified = last_modified_of(wasm_path);
+    let js_last_modified = last_modified_of(js_path);
+
+    debug!("wasm last modified: {}", wasm_last_modified);
+    debug!("js last modified:   {}", js_last_modified);
+
+    let last_compile = if wasm_last_modified < js_last_modified {
+        wasm_last_modified
+    } else {
+        js_last_modified
+    };
+
+    debug!("chapter last modified: {}", chapter_last_modified);
+    debug!("last compile:          {}", last_compile);
+
+    &last_compile < chapter_last_modified
+}
+
+fn last_modified_of(p: impl AsRef<Path>) -> DateTime<Utc> {
+    let system_time = fs::metadata(p)
+        .expect("Cannot read metadata of file")
+        .modified()
+        .expect("Cannot read last modified of file");
+    DateTime::<Utc>::from(UNIX_EPOCH + system_time.duration_since(UNIX_EPOCH).unwrap())
 }
 
 #[allow(unused)]
